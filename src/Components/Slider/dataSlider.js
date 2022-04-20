e06ba559bd220b4f046fe8f0133f8b669e788e17
shariquearshad <shariquearshad@gmail.com>
2022-04-20 20:36:09 +0530

    Initial commit

---
+++ src/Components/Slider/dataSlider.js
@@ -0,0 +1,26 @@
+import { v4 as uuidv4 } from "uuid";
+
+const dataSlider = [
+  {
+    id: 1,
+    title: "Lorem ipsum",
+    subTitle: "Lorem",
+    img:"img1.jpeg"
+  },
+  {
+    id: 2,
+    title: "Lorem ipsum",
+    subTitle: "Lorem",
+    img:"img2.jpeg"
+  },
+  {
+    id: 3,
+    title: "Lorem ipsum",
+    subTitle: "Lorem",
+    img:"map (2).jpg",
+    link:"https://www.google.com/maps/place/Mistika+Banquet,+Eco+Park/@22.5951671,88.4703251,15z/data=!4m5!3m4!1s0x0:0xd4be211a72c54e2b!8m2!3d22.595111!4d88.4702282"
+  }
+  
+];
+
+export default dataSlider;
