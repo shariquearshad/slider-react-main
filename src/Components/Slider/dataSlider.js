2ea0e4b8f786072e09dff8d268ce95c98af04ef0
shariquearshad <shariquearshad@gmail.com>
2022-04-22 15:46:53 +0530

    img added

--- src/Components/Slider/dataSlider.js
+++ src/Components/Slider/dataSlider.js
@@ -17,7 +17,7 @@ const dataSlider = [
     id: 3,
     title: "Lorem ipsum",
     subTitle: "Lorem",
-    img:"map (2).jpg",
+    img:"map.jpg",
     link:"https://www.google.com/maps/place/Mistika+Banquet,+Eco+Park/@22.5951671,88.4703251,15z/data=!4m5!3m4!1s0x0:0xd4be211a72c54e2b!8m2!3d22.595111!4d88.4702282"
   }
   
